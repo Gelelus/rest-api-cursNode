bb9bf53d96e10fbb7da20f231d8140118e4d945e
Gelelus <mundor7392290@gmail.com>
2020-10-13 10:37:17 +0300

    delete unnessesary

--- src/helpers/errorStatus.js
+++ src/helpers/errorStatus.js
@@ -1,6 +1,3 @@
 module.exports = function status(e) {
-  if (typeof e.message === 'string') {
-    return e.message.includes("doesn't exist") ? 404 : 400;
-  }
-  return 400;
+  return e.message.includes("doesn't exist") ? 404 : 400;
 };
