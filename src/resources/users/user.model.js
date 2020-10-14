cb6783c4c7d5bd2c7745faf7fd9855b3e5382ea9
Gelelus <mundor7392290@gmail.com>
2020-10-14 15:00:24 +0300

    fixisng

--- src/resources/users/user.model.js
+++ src/resources/users/user.model.js
@@ -1,75 +1,37 @@
-const uuid = require('uuid');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
-const { ErrorHandler } = require('../../helpers/customError.js');
-const Task = require('../tasks/task.model');
-
-const usersMemory = [
-  // {
-  //   id: 'testId123',
-  //   name: 'testName',
-  //   login: 'testLogin',
-  //   password: 'test12456789'
-  // }
-];
-
-class User {
-  constructor(
-    { name = 'noName', login = 'noName', password = '123', id },
-    update = false
-  ) {
-    if (update) {
-      this.id = id;
-    } else {
-      this.id = uuid();
-    }
-
-    this.name = name;
-    this.login = login;
-    this.password = password;
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    default: 'Nameless',
+    trim: true
+  },
+  login: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true
   }
+});
 
-  static find() {
-    return usersMemory.map(a => ({
-      id: a.id,
-      name: a.name,
-      login: a.login
-    }));
+// userSchema.pre("remove", function (next) {
+//   Task.updateMany({ userId: this._id }, { $set: { userId: null } },{ multi: true }).exec();
+//   next();
+// });
+
+userSchema.pre('save', async function test(next) {
+  const user = this;
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8);
   }
+  next();
+});
 
-  static findById(id) {
-    return usersMemory.find(a => {
-      return a.id === id;
-    });
-  }
-
-  static findByIdAndUpdate(data, id) {
-    for (let i = 0; i < usersMemory.length; i++) {
-      if (usersMemory[i].id === id) {
-        usersMemory[i] = new User({ ...data, id }, true);
-        return usersMemory[i];
-      }
-    }
-
-    throw new ErrorHandler(404, "user doesn't exist");
-  }
-
-  static findByIdAndDelete(id) {
-    for (let i = 0; i < usersMemory.length; i++) {
-      if (usersMemory[i].id === id) {
-        usersMemory.splice(i, 1);
-        Task.deleteUserRelationship(id);
-        return { message: `user with id - ${id} was deleted` };
-      }
-    }
-
-    throw new ErrorHandler(404, "user doesn't exist");
-  }
-
-  static insert(obj) {
-    const user = new User(obj);
-    usersMemory.push(user);
-    return user;
-  }
-}
-
+const User = mongoose.model('User', userSchema);
 module.exports = User;
